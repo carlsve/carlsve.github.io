ced6426deb2e2fca0e8d2da06d378f232383d63e
karlPet <karlpetter.svensson@hotmail.se>
2016-12-02 11:16:19 +0100

    fixed jquery logic

--- flip-card.js
+++ flip-card.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
 
-    $(".back a").hide();
+    let mouseLeaveFlag = true;
+    const FADE_TIME = 500;
+
+    $(".back").hide();
 
     $("#card").bind("click", function() {
+        toggleMouseLeaveFlag();
         $("#card").toggleClass('flipped');
-        $(".front a").fadeToggle(1000);
-
-        $(".back a").fadeToggle(1000);
+        $(".front").fadeToggle(FADE_TIME);
+        $(".back").fadeToggle(FADE_TIME);
+        setTimeout(toggleMouseLeaveFlag, 2*FADE_TIME);
+    }).bind("mouseleave", function() {
+        if (mouseLeaveFlag) {
+            $("#card").removeClass('flipped');
+            $(".front").fadeIn(FADE_TIME);
+            $(".back").fadeOut(FADE_TIME);
+        }
     });
+
+    function toggleMouseLeaveFlag() {
+        mouseLeaveFlag = !mouseLeaveFlag;
+    }
 });
