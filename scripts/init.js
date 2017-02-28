4a92cde5f084e9af1101190ad3eb5a580a9c7b32
karlpet <karlpetter.svensson@hotmail.com>
2017-02-28 19:20:46 +0100

    temp

---
+++ scripts/init.js
@@ -0,0 +1,55 @@
+/*
+ *  Add event listener to the mobile screen menu button.
+ *  content: toggle .deactivated
+ *  header: toggle .menu-opened
+ *  menu_icon: toggle activated
+ */
+var burgerContainer = document.querySelector('#burger-container');
+
+burgerContainer.onclick = function () {
+
+    var header = document.querySelector(".header");
+    var menu_icon = document.querySelector("#menu_icon");
+    var content = document.querySelector(".content");
+
+    header.classList.toggle("menu-opened");
+    menu_icon.classList.toggle("activated");
+    content.classList.toggle("deactivated");
+};
+
+/* ------ LOW-RES LOAD-IN IMAGE HANDLER ------ */
+// gradually loads in
+function upgradeImage(object) {
+    var hero = document.getElementsByClassName("hero")[0];
+
+    if (object.id === "load-in") {
+        hero.classList.add("loaded");
+    } else if (object.id === "load-in-big") {
+        hero.classList.add("loaded_big")
+    }
+    hero.classList.remove("blur_on_load");
+}
+
+
+
+/*-------SCROLLBAR GRADIENT FIX -------
+
+bug-description: the gradient that makes the
+    medium text vanish on scroll, is visible and
+    can look bad during resize.
+
+fix: on window resize event, add class 'noshow'
+    which sets display to none.
+*/
+var visibilityTimer;
+
+window.addEventListener('resize', function () {
+    clearTimeout(visibilityTimer);
+
+    var gradient = document.getElementById('gradient');
+    gradient.style.visibility = 'hidden';
+
+    visibilityTimer = setTimeout(() => {
+        gradient.style.visibility = 'visible';
+    }, 1000);
+}, true)
