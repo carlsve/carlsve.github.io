da37d954edf59c1e4ea88562b5de712e192d360b
Karl Petter Svensson <karlpet@users.noreply.github.com>
2018-03-19 22:12:08 +0100

    Create index.js

---
+++ DOMreact/lib/index.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function() {
+  var root = document.getElementById('root')
+
+  root.innerHTML =
+    div(
+      h1({ id: 'helloo', className: 'wasup', data: {hello:'hello'} },'outerheader', p('Lite mera text som ligger i h1?')),
+      div(
+        h1('header')),
+        p('lorem ipsum'),
+        a({ href: 'https://google.com' },'google')
+      ),
+      div({ className: 'hellp'}, 'hjälp mig')
+})
